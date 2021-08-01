1d5c3c21ec372bcf6bcda99fb2b17419f4883146
John Tso <tsotcj@gmail.com>
2021-07-31 21:03:45 -0700

    Fix bug of not checking the num itself. Descriptions added

--- factor/get_factor.py
+++ factor/get_factor.py
@@ -1,7 +1,14 @@
 def get_factor(num):
+    """
+    Get a list of factors of a number.
+    >>> get_factor(2)
+    [1, 2]
+    >>> get_factor(12)
+    [1, 2, 3, 4, 6, 12]
+    """
 
     factors = []
-    for i in range(1, num):
+    for i in range(1, num+1):
         if not num % i:
             factors.append(i)
 
