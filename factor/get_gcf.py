a0789428d970a39aa59289f4ea3f83be3a12f2f5
John Tso <tsotcj@gmail.com>
2021-08-03 10:28:20 -0700

    New extension to the get_gcf function(return a list of common factor if second arguement is True else return gcf)

--- factor/get_gcf.py
+++ factor/get_gcf.py
@@ -1,17 +1,22 @@
 from get_factor import get_factor
 
-def get_gcf(*nums):
+# gcf stands for get common factor or greatest common factor
+def gcf(nums, show_all=False):
     """
-    Given a list of numbers, return their Greatest Common Factor (GCF).
-    >>> get_gcf([2, 4])
+    Given a list of numbers, return their greatest common factor or get common factor (GCF).
+    >>> gcf([2, 4])
     2
-    >>> get_gcf([4, 16, 8])
+    >>> gcf([4, 16, 8])
     4
+    >>> gcf([4, 16, 8], show_all=True)
+    [1, 2, 4]
+    >>> gcf([12, 24], True)
+    [1, 2, 3, 4, 6, 12]
     """
 
     #  Get the minimum number of nums
     m = min(nums)
-    gcf = 1
+    gcf = []
 
     # Iterate factors of the minimum number
     for i in get_factor(m):
@@ -19,5 +24,9 @@ def get_gcf(*nums):
             if n % i != 0:
                 break
         else:
-            gcf = i
-    return gcf
+            gcf.append(i)
+
+    # if show_all is True, return a list of common factors, else the greatest common factor
+    return gcf if show_all else gcf[-1] 
+
+print(gcf([12, 24]))
